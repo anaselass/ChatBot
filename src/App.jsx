b47e81fbff91eb0a655abedb9b4ee55ad829339e
anaselass <anas.elassri.01@gmail.com>
2025-05-13 11:31:58 +0100

    feat: initialize chatbot application with React and Vite
    - Added package.json to manage dependencies and scripts for development.
    - Included Vite logo SVG in the public directory.
    - Created main application component (App.jsx) with chatbot UI structure.
    - Developed ChatBotIcon component for chatbot representation.
    - Styled application with index.css for a clean and modern look.
    - Set up entry point (main.jsx) for rendering the application.
    - Configured Vite with vite.config.js for React support.

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import ChatBotIcon from "./components/ChatBotIcon";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/* Chat bot header */}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatBotIcon />
+            <h2 className="logo-text">chatbot</h2>
+          </div>
+          <button class="material-symbols-outlined">keyboard_arrow_down</button>
+        </div>
+
+        {/* Chat bot body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatBotIcon />
+            <p className="message-text">
+              hey there 👏 <br /> how can I help you today?
+            </p>
+          </div>
+          <div className="message user-message">
+            <ChatBotIcon />
+            <p className="message-text">
+              Lorem ipsum dolor sit amet. consectetur adipisicing elit. Quisquam
+            </p>
+          </div>
+        </div>
+
+        {/* Chat bot footer */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input
+              type="text"
+              placeholder="Message..."
+              className="message-input"
+              required
+            />
+            <button class="material-symbols-outlined">keyboard_arrow_up</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
